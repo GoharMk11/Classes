1edbe9bb91ba93be0a680d6e8254c275a53d8371
Gohar <gohar.mkrtchiian@gmail.com>
2025-11-21 22:42:18 +0400

    ex6

---
+++ Lesson_11_15/Lesson_11_15_Ex6/Program.cs
@@ -0,0 +1,19 @@
+﻿// Հաշվել 1ից մինչև 100 թվերի միջին թվաբանականը։
+using System;
+namespace lesson_2
+{
+    class ex6
+    {
+        static void Main()
+        {
+            int sum = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                sum += i;
+            }
+            int count = 100;
+            double mean = (double)sum / count;
+            Console.WriteLine($"The mean is {mean}");
+        }
+    }
+}
