36794c717a0125b8a3213fec39848af580e006a2
Gohar <gohar.mkrtchiian@gmail.com>
2025-11-21 22:36:23 +0400

    Ex5

--- Lesson_11_15/Lesson_11_15_5/Program.cs
+++ Lesson_11_15/Lesson_11_15_5/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Տրված է a=60 , հաշվել y=2sin(a) արտահայտության արժեքը:
+using System;
+namespace lesson_2
+{
+    class ex5
+    {
+        static void Main()
+        {
+            int a = 60;
+            double radians = a * Math.PI / 180;
+            double b = 2 * Math.Sin(radians);
+            Console.WriteLine("The answer is " + b);
+        }
+    }
+}
