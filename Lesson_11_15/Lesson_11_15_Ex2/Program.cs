c629665d1b00b78d6de0770af14fb2a120404efa
Gohar <gohar.mkrtchiian@gmail.com>
2025-11-21 21:57:23 +0400

    Ex2

--- Lesson_11_15/Lesson_11_15_Ex2/Program.cs
+++ Lesson_11_15/Lesson_11_15_Ex2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Տրված է x=2.5 թիվը։ Հաշվեք y= -6x + 8 արտահայտության արժեքը:
+using System;
+namespace lesson_2
+{
+    class ex2
+    {
+        static void Main()
+        {
+            double x = 2.5;
+            double y = -6 * x + 8;
+            Console.WriteLine(y);
+        }
+    }
+}
