b4b58f064f97009868a218a7bf9584b80191aea9
Gohar <gohar.mkrtchiian@gmail.com>
2025-11-21 22:30:06 +0400

    changs

---
+++ Lesson_11_18/Lesson_11_15_Ex2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Տրված է x=2.5 թիվը։ Հաշվեք y= -6x + 8 արտահայտության արժեքը:
+using System;
+namespace lesson_2
+{
+    class ex2
+    {
+        static void Main()
+        {
+            double x = 2.5;
+            double y = -6 * x + 8;
+            Console.WriteLine(y);
+        }
+    }
+}
