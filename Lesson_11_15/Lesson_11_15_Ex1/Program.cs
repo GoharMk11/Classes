46647596bc700bfa680e8691c1aab4cff34ded48
Gohar <gohar.mkrtchiian@gmail.com>
2025-11-21 21:53:46 +0400

    EX_1

--- Lesson_11_15/Lesson_11_15_Ex1/Program.cs
+++ Lesson_11_15/Lesson_11_15_Ex1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Տրված են a և b թվերը։ Հաշվեք դրանց միջին թվաբանականը։
+using System;
+namespace lesson_2
+{
+    class ex1
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter the 1st value");
+            string first = Console.ReadLine();
+            Console.WriteLine("Please enter the 2nd value");
+            string second = Console.ReadLine();
+            float a = float.Parse(first);
+            float b = float.Parse(second);
+            float c = (a + b) / 2;
+            Console.WriteLine("The answer is " + c);
+        }
+    }
+}
