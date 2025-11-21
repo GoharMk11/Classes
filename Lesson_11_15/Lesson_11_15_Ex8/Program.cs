a8101915e41a47d982e1a93d8855461d1e7d80ce
Gohar <gohar.mkrtchiian@gmail.com>
2025-11-21 23:34:58 +0400

    Ex8

---
+++ Lesson_11_15/Lesson_11_15_Ex8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lesson_2
+{
+    class ex8
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter the value for a:");
+            string first = Console.ReadLine();
+            double a = double.Parse(first);
+            if (a == 0)
+            {
+                Console.WriteLine("a cannot be 0, the function is not defined.");
+                return; // stop Main here
+            }
+            Console.WriteLine("Please enter the value for x:");
+            string second = Console.ReadLine();
+            double x = double.Parse(second);
+            double result = Function1(a, x);
+            Console.WriteLine("The result is " + result);
+        }
+        static double Function1(double a, double x)
+        {
+            double y = (a * Math.Pow(x, 3)) + (Math.Pow(x,1.0 / a));
+            return y;
+        }
+    }
+}
