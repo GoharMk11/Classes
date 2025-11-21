d4e2bd6444dcfd7d5e6164891ec4b9443e35c874
Gohar <gohar.mkrtchiian@gmail.com>
2025-11-21 22:52:30 +0400

    Ex7

---
+++ Lesson_11_15/Lesson_11_15_Ex7/Program.cs
@@ -0,0 +1,20 @@
+﻿//Գտնել 1ից մինչև 100 միջակայքում կենտ թվերի քանակը։
+using System;
+namespace lesson_2
+{
+    class ex7
+    {
+        static void Main()
+        {
+            int count = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"The count is {count}");
+        }
+    }
+}
