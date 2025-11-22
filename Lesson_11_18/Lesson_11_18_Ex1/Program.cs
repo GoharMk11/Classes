62fd61273d62b425b99c3144e0e414323f1057a5
Gohar <gohar.mkrtchiian@gmail.com>
2025-11-23 03:28:07 +0400

    Ex1

--- Lesson_11_18/Lesson_11_18_Ex1/Program.cs
+++ Lesson_11_18/Lesson_11_18_Ex1/Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1 չափանի զանգված - Տրված n երկարության զանգվածում գտնել առավելագույնը և նվազագույնը։
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Security.Cryptography;
+namespace Lesson_3
+{
+    class Ex1
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter the length of the array");
+            int n = int.Parse(Console.ReadLine());
+            if(n<=0)
+            {
+                Console.WriteLine("Please enter a positive number");
+                return;
+            }
+            int[] numbers = new int[n];
+            Random rnd = new Random(); //Its random generator
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = rnd.Next(0, 101);
+             }
+            Console.Write("Array: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(numbers[i]);
+                if (i != n - 1)
+                {
+                    Console.Write(", ");
+                }
+                else Console.Write(":");
+            }
+            Console.WriteLine();             
+            int min;
+            int max;
+            FindMinMax(numbers, out min, out max);
+            Console.WriteLine("Minimal value: " + min);
+            Console.WriteLine("Maximal value: " + max);
+
+        }
+
+
+        static void FindMinMax(int[] numbers, out int min, out int max)
+        {
+             min = numbers[0];
+             max = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] < i)
+                {
+                    min = numbers[i];
+                }
+                if (numbers[i] > i)
+                {
+                    max = numbers[i];
+                }
+            }
+
+        }
+    }
+}
+
