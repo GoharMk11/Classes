ab92bc94694a61fc1619da1fb5292fcf07a7f4cb
Gohar <gohar.mkrtchiian@gmail.com>
2025-11-24 01:53:10 +0400

    Ex1

---
+++ Lesson_11_20/Lesson_11_20_Ex1/Program.cs
@@ -0,0 +1,49 @@
+﻿// Անհրաժեշտ է գրել մեթոդը, որը ստանում է ամբողջ թվերի զանգված և
+// ստեղծում է նոր string որի էլեմենտները ստորակետով բաժանված են:
+
+
+using System;
+namespace Lesson_3
+{
+    class Ex1
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter a number");
+            int n = int.Parse(Console.ReadLine());
+            if (n<=0)
+            {
+                Console.WriteLine("PLease enter a positive number");
+                return;
+            }
+            int[] ar = new int[n];
+            Random rnd = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                ar[i] = rnd.Next(0, 101);
+            }
+            string text = ArrayToString(ar);
+            Console.WriteLine(text);
+
+        }
+        static string ArrayToString(int[] ar)
+        {
+            string result = "";
+            for (int i=0;i<ar.Length;i++)
+            {
+                if (i>0)
+                {
+                    Console.Write(", ");
+                    
+                }
+                Console.Write(ar[i]);
+                if (i == ar.Length - 1)
+                {
+                    Console.Write(":");
+                }
+            }
+            return result;
+        }
+    }
+}
