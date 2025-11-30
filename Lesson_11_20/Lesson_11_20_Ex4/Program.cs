7eb909bf5e9d627f6029041f9efa1474081e0318
Gohar <gohar.mkrtchiian@gmail.com>
2025-11-30 22:23:04 +0400

    Improvement

--- Lesson_11_20/Lesson_11_20_Ex4/Program.cs
+++ Lesson_11_20/Lesson_11_20_Ex4/Program.cs
@@ -1,7 +1,6 @@
 ﻿// Անհրաժեշտ է ստեղծել Point անունով struct տիպ, որը կունենա երկու դաշտ՝ X և Y։
-// Այնուհետև պետք է ստեղծել 10 էլեմենտից բաղկացած զանգված այս struct տիպով և լցնել
-// այն պատահական (Random) X և Y արժեքներով։ Վերջում անհրաժեշտ է ստեղծել նոր զանգված,
-// որտեղ Point-երը կլինեն դասակարգված աճման կարգով՝ նախ ըստ X-ի,իսկ եթե X-երը հավասար են՝ ըստ Y-ի։
+// Այնուհետև պետք է ստեղծել 10 էլեմենտից բաղկացած զանգված այս struct տիպով և լցնել այն պատահական (Random) X և Y արժեքներով։
+// Վերջում պետք է ստեղծել նոր զանգված,որտեղ Point-երը կլինեն դաս․ աճման կարգով՝ նախ ըստ X-ի,իսկ եթե X-երը հավասար են՝ ըստ Y-ի։
 
 using System;
 namespace Lesson_3
@@ -10,49 +9,66 @@ namespace Lesson_3
     {
         public int X;
         public int Y;
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
     }
-
-
     class Ex4
     {
         static void Main()
         {
-            Point[] points = new Point[10];
+            int count = 10;
+            Point[] mainpoints= FillingPoint(count);
+            Console.WriteLine("\n----- SORTED -----");
+            Point[] sortedPoints = SortingPoints(mainpoints);
+            PrintPoints(sortedPoints);
+        }
+        static Point[] SortingPoints(Point[] arr)
+        {
+            Point[] sorted = new Point[arr.Length];
+            for (int i=0;i<arr.Length;i++)
+            {
+                sorted[i] = arr[i];
+            }
+
+            for (int i=0;i<sorted.Length;i++)
+            {
+                for (int j=0;j<sorted.Length-i-1;j++)
+                {
+                    if (sorted[j].X > sorted[j+1].X || sorted[j].X == sorted[j + 1].X && sorted[j].Y > sorted[j+1].Y)
+                    {
+                        Point temp = sorted[j];
+                        sorted[j] = sorted[j+1];
+                        sorted[j + 1] = temp;
+                    }
+                }
+
+            }
+            return sorted;
+        }
+        static void PrintPoints(Point[] points)
+        {
+            for (int i = 0; i < points.Length; i++)
+                Console.WriteLine(points[i]); 
+        }
+        static Point[] FillingPoint(int count)
+        {
+            Point[] points = new Point[count];
             Random rdm = new Random();
-            Console.WriteLine("Original points:");
             for (int i = 0; i < points.Length; i++)
             {
                 points[i].X = rdm.Next(1, 101);
                 points[i].Y = rdm.Next(1, 101);
                 Console.WriteLine($"Point {i}: X = {points[i].X}, Y = {points[i].Y}");
             }
-            Point[] sortedPoints = new Point[10];
-            for (int i = 0; i < points.Length; i++)
-            {
-                sortedPoints[i] = points[i];
-            }
-            for (int i = 0; i < sortedPoints.Length; i++)
-            {
-                for (int j = i + 1; j < sortedPoints.Length; j++)
-                {
-                    Point a = sortedPoints[i];
-                    Point b = sortedPoints[j];
 
-                    if (a.X > b.X || (a.X == b.X && a.Y > b.Y))
-                    {
-                        Point temp = sortedPoints[i];
-                        sortedPoints[i] = sortedPoints[j];
-                        sortedPoints[j] = temp;
-                    }
-                }
-                
-            }
-            Console.WriteLine("Sorted points:");
-            for (int i = 0; i < sortedPoints.Length; i++)
-            {
-                Console.WriteLine($"Point {i}: X = {sortedPoints[i].X}, Y = {sortedPoints[i].Y}");
-            }
-
+            return points;
         }
-    }
+    }  
 }
