478e3f69f208c1ec66c37e03a2fee59d3b42365b
Gohar <gohar.mkrtchiian@gmail.com>
2025-12-01 19:25:23 +0400

    Ex1

---
+++ Lesson_11_22/Lesson_11_22_Ex1/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StudentsModel
+{
+    struct Student
+    {
+        public string Name { get; set; }
+        public int[] Scores { get; set; }
+        public Student(string name,int[] scores)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Name can't be empty.", nameof(name));
+            if (scores == null || scores.Length == 0)
+                throw new ArgumentException("Scores can't be empty.", nameof(scores));
+            Name = name.Trim(); 
+            Scores = scores;
+        }
+    }
+}
