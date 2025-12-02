9049ba390e0baec8d5bf2bc2745e2c3dafe972fa
Gohar <gohar.mkrtchiian@gmail.com>
2025-12-02 14:52:52 +0400

    Ex2

--- Lesson_11_22/Lesson_11_22_Ex2/MatrixBuilder.cs
+++ Lesson_11_22/Lesson_11_22_Ex2/MatrixBuilder.cs
@@ -1,8 +1,32 @@
 ﻿using System;
+using System.Diagnostics.Metrics;
+using System.Threading.Tasks;
+
+/*
+
+Ask: “Enter number of rows (n):”
+
+Ask: “Enter number of columns (m):”
+
+Validate:
+
+if user types not-a - number → ask again
+
+if n <= 0 or m <= 0 → ask again*/
 
 namespace MatrixApp
 {
-    internal class MatrixBuilder
+    public class MatrixBuilder
     {
+        public static int[,] CreateMatrix()
+        {
+            Console.WriteLine("Enter number of rows (n)");
+
+            if (!int.TryParse(Console.ReadLine(), out int n)
+                {
+                    Console.WriteLine("Plese enter a number!")
+                }
+            Console.WriteLine("Enter number of columns (m)");
+        }
     }
 }
---
+++ Lesson_11_22/Lesson_11_22_Ex2/ReadPositiveInt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MatrixApp
+{
+    public class ReadInt
+    {
+        public static int ReadPositiveInt(string prompt)
+        {
+            while (true)
+            {
+                Console.Write(prompt);
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out int number) && number > 0)
+                    return number;
+                Console.WriteLine("Please enter a positive number.");
+            }
+        }
+    }
+}
