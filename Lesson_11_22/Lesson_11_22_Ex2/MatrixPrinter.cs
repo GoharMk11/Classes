1fcaddb9d6b963115184270a95587fcac1774f0a
Gohar <gohar.mkrtchiian@gmail.com>
2025-12-02 15:45:41 +0400

    Ex2

--- Lesson_11_22/Lesson_11_22_Ex2/MatrixPrinter.cs
+++ Lesson_11_22/Lesson_11_22_Ex2/MatrixPrinter.cs
@@ -1,10 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace MatrixApp
 {
-    internal class MatrixPrinter
+    public class MatrixPrinter
     {
+        public static void Print(int[,] matrix, string title)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (!String.IsNullOrEmpty(title))
+            {
+                sb.AppendLine(title);
+            }
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            for (int i=0;i<rows;i++)
+            {
+                for (int j=0;j<cols;j++)
+                {
+                    
+                    sb.Append(matrix[i,j]);
+                    if (j != cols - 1)
+                    {
+                        sb.Append(", ");
+                    }                 
+                    
+                }
+                sb.AppendLine();
+            }
+            Console.WriteLine(sb.ToString());
+        }
     }
 }
