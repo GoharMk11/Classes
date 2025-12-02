f5c445d55162ea17ec3efebfdb0b9c723859d699
Gohar <gohar.mkrtchiian@gmail.com>
2025-12-02 14:23:03 +0400

    Ex2

--- Lesson_11_22/Lesson_11_22_Ex2/MatrixBuilder.cs
+++ Lesson_11_22/Lesson_11_22_Ex2/MatrixBuilder.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace Lesson_11_22_Ex2
+namespace MatrixApp
 {
     internal class MatrixBuilder
     {
--- Lesson_11_22/Lesson_11_22_Ex2/MatrixPrinter.cs
+++ Lesson_11_22/Lesson_11_22_Ex2/MatrixPrinter.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Lesson_11_22_Ex2
+namespace MatrixApp
 {
     internal class MatrixPrinter
     {
